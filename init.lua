608c37e64f6e92fbe7c98c36a51feef5d9741699
Daniel Wuensche <daniel@dvoid.xyz>
2025-07-29 22:30:01 +0200

    load config files

--- init.lua
+++ init.lua
@@ -2,4 +2,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
 
+require("config.options") -- load package manager
+require("config.keymaps") -- load package manager
+require("config.autocmd") -- load package manager
+
 require("config.lazy") -- load package manager
+
+require("config.autocmd_lsp") -- load package manager
