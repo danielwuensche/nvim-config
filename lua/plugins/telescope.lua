edb3477443bbfd98acecdfb2922c062f4bccc0d6
Daniel Wuensche <daniel@dvoid.xyz>
2025-07-31 21:03:18 +0200

    fix telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,17 @@
-return {
+return { -- Fuzzy Finder (files, lsp, etc)
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
 	event = "VimEnter",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-		cond = function()
-			return vim.fn.executable("make") == 1
-		end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
 		require("telescope").setup({
@@ -18,6 +21,7 @@ return {
 				},
 			},
 		})
+		-- Enable Telescope extensions if they are installed
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
 
